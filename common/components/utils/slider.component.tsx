a2d964271be727cb575714a98ab7ae7b091f6bc3
Zulnabil Faiz Moh. Alhadad <zulnabil.22@gmail.com>
2021-08-26 08:34:53 +0700

    feat: created modular slider component

---
+++ common/components/utils/slider.component.tsx
@@ -0,0 +1,48 @@
+import { FC, MouseEvent, useCallback, useRef, useState } from "react"
+
+const Slider: FC = ({ children }) => {
+  const sliderReference = useRef<HTMLDivElement>(null)
+  const [isScrolling, setScrolling] = useState(false)
+  const [clientX, setClientX] = useState(0)
+  const [scrollX, setScrollX] = useState(0)
+
+  const handleMouseDown = useCallback(
+    (event: MouseEvent<HTMLElement>) => {
+      setScrolling(true)
+      setClientX(event.clientX)
+    },
+    [setScrolling, setClientX]
+  )
+
+  const handleMouseUp = useCallback(() => {
+    setScrolling(false)
+  }, [setScrolling])
+
+  const handleMouseMove = useCallback(
+    (event: MouseEvent<HTMLElement>) => {
+      if (isScrolling && sliderReference.current) {
+        const scrollLeft = scrollX + clientX - event.clientX
+        sliderReference.current.scrollLeft = scrollLeft
+        setScrollX(scrollLeft)
+        setClientX(event.clientX)
+      }
+    },
+    [isScrolling, sliderReference, scrollX, clientX, setScrollX, setClientX]
+  )
+
+  return (
+    <div
+      ref={sliderReference}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      className={`slider-wrapper flex gap-8 w-full whitespace-nowrap overflow-auto ${
+        isScrolling ? "cursor-grabbing" : "cursor-grab"
+      }`}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Slider
