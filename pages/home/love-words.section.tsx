65dd0721c79556c9e8820d1bd7686fb10f483025
hanhanlie <hanhan.lie12@gmail.com>
2021-08-30 21:25:54 +0700

    feat: love word vectors responsive

--- pages/home/love-words.section.tsx
+++ pages/home/love-words.section.tsx
@@ -3,8 +3,8 @@ import type { LoveWordsProps } from "./types/love-words.type"
 
 const SectionLoveWords: FC<LoveWordsProps> = ({ text }) => {
   return (
-    <section className="bg-green-gradient relative text-center p-8 md:p-32 sm:p-16">
-      <p className="font-medium text-xl d:text-4xl sm:text-2xl leading-snug sm:leading-normal text-white">
+    <section className="quote-wrapper bg-green-gradient relative text-center p-8 md:p-32 sm:p-16">
+      <p className="font-medium text-lg md:text-4xl sm:text-2xl leading-snug sm:leading-normal text-white">
         {text}
       </p>
       <hr className="w-3/4 m-auto md:mt-10 mt-5 opacity-50" />
